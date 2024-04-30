f0f9eab706216b758e2c65d731da143ec4c5e321
cdjesse <102725583@student.swin.edu.au>
2024-04-30 10:25:33 +1000

    Added offset for cursor position

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -51,7 +51,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         Vector2 direction = mousePosition - transform.position;
-        float angle = Vector2.SignedAngle(Vector2.right, direction);
+        float angle = Vector2.SignedAngle(Vector2.right, direction) + 270f;
         transform.eulerAngles = new Vector3(0, 0, angle);
     }
 }
